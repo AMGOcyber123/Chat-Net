7a35dcf08cc30b8c46676aa3838da8c070d0e937
AMGOcyber123 <aayumodi123@gmail.com>
2022-12-12 17:36:31 +0530

    socket files

---
+++ public/js/chat.js
@@ -0,0 +1,75 @@
+const socket = io() // Establish connection 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+socket.on('message', (message) => {
+    // console.log(message.text)
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm A')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm A')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')  
+        })
+    })
+})
+
+
+// socket.on('countupdate', (count)=>{  
+//     console.log('Count is updated !'+(count++));
+// })
+
+
+// document.querySelector('#increment').addEventListener('click' , () => {
+//     console.log('clicked');
+//     socket.emit('increment')
+// })
---
+++ src/index.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words') // Filter profane language 
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+
+// created explicitly , because socket expects raw http server 
+// express does it behind the scene but doesnt allow to pass it
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', generateMessage('Welcome!'))  // cmd , var
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {                   // send location using (lat , long)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
+
+
+
+// server emit -> client receive - countupdate 
+// client emit -> server receive - increment
+
+
+
+// // .on()   this is done for receiver end 
+// io.on('connection', (socket) =>{
+//     console.log('New websocket connection');
+//     socket.emit('countupdate', count)       // .emit()   this is done for senders end 
+//     socket.on('increment',()=>{
+//         count++;
+//         //server.emit('countupdate', count)
+
+//         io.emit('countupdate', count)       // to increase limit to more than 1 client and show updation in every client end 
+//     } )
+// })
+
+// .on()   this is done for receiver end 
